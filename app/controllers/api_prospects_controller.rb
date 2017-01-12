0b4e4e0cbfa7f34fa1fee2a22e444128a2a2a28d
Hinesh Patel <hineshpatel0@googlemail.com>
2017-01-12 10:59:24 +0000

    Task A

---
+++ app/controllers/api_prospects_controller.rb
@@ -0,0 +1,42 @@
+require 'httparty'
+
+class ApiProspectsController < ApplicationController
+  def index
+    @prospects = get_response('http://tuskermarvel.com/prospects.json')
+    render json: @prospects
+  end
+
+  def packages
+    @packages = get_response('http://tuskermarvel.com/packages.json')
+    render json: @packages
+  end
+
+  def packagesCity
+    @prospects = get_response('http://tuskermarvel.com/prospects.json')
+    @packages = get_response('http://tuskermarvel.com/packages.json')
+    @contents = []
+
+    @prospects.each do |prospects|
+      if prospects["delivery_city"] == params[:city]
+        prospects["received"].each do |received|
+          r = received
+          @packages.each do |packages|
+            packages["contents"].each do |content|
+              if r == content
+                @contents.push(packages["code"])
+              end
+            end
+          end
+        end
+      end
+    end
+    render json: @contents
+  end
+
+  private
+
+  def get_response(url)
+    response = HTTParty.get(url)
+    @output = JSON.parse(response.body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'api_prospects#index'
+  get '/packages' => 'api_prospects#packages'
+  post '/packages_city' => 'api_prospects#packagesCity'
 end
